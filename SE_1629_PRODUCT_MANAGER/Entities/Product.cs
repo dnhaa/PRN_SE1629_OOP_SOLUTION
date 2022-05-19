a9dd7e623fcd020f5529da950cf8dab10965df3f
dnhaa <elnganha@gmail.com>
2022-05-19 09:43:38 +0700

    commit project product management the first time

---
+++ SE_1629_PRODUCT_MANAGER/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿namespace Prn.Se1629;
+public class Product
+{/*Define property*/
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public string Desc { get; set; }
+    public double UnitPrice { get; set; }
+    public int Quantity { get; set; }
+
+   /*Constructor*/
+   public Product()
+    {
+
+    }
+
+    public Product(int id, string productName, string desc, double unitPrice, int quantity)
+    {
+        Id = id;
+        ProductName = productName;
+        Desc = desc;
+        UnitPrice = unitPrice;
+        Quantity = quantity;
+    }
+
+    public override string? ToString() => $"[Id = {Id}, Product Name = {Product}, Desc = {Desc}, Unit Price = {UnitPrice}, Quantity = {Quantity}]";
+
+    /*Increase percent unit_price*/
+    public void IncreaseUnitPrice(double unitPrice) => UnitPrice = UnitPrice * unitPrice / 100;
+}
