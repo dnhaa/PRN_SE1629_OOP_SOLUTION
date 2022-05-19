8a0bfb768c91e1423fba174a07576d126e90114e
dnhaa <elnganha@gmail.com>
2022-05-19 09:51:28 +0700

    commit manager

---
+++ SE_1629_PRODUCT_MANAGER/Manager/iProduct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prn.Se1629;
+
+internal interface iProduct
+{
+}
